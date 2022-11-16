974e0ff9684dc869da472cb8cae92aca3a187316
shireesha <ps.shireesha@gmail.com>
2022-11-16 17:14:14 +0000

    initial setup

---
+++ modules/property-details/helpers.js
@@ -0,0 +1,3 @@
+sincePurchase = `recentValuation - originalPurchasePrice`
+sincePurchasePercentage = `sincePurchase / originalPurchasePrice * 100`
+annualAppreciation =`sincePurchasePercentage / number of years since purchase`
