93adfa56682eef34842371aaf46a0f054681933b
chioranionutcatalin <chioranionutcatalin@gmail.com>
2022-11-21 20:52:38 +0200

    some final touches and some changes examples

--- modules/property-details/index.js
+++ modules/property-details/index.js
@@ -9,31 +9,75 @@ import {
 import axios from 'axios';
 import PropTypes from 'prop-types'; // ES6
 
-  //TO.DO: create a defs file
+//TO.DO: It feels like the jsx tree/code is to verbose the components can be simplified and can we can reduce the code duplication by creating some section components like <PropertyDetails /> <ValuationChange/> and <Mortgage/>
+//TO.DO: Instead of using the AccountList and AccountListItem I would create something more general and not business related components like only a Layout and a LayoutItem component and based on that create multy dimentional matrix layout or something like row, column(s) maybe would be more clean.
+//TO.DO  For more minimized layout you could group the data in structures like arrays and loop over them to minimize the code in the jsx. it would require to many changes now for this component since you have that price bolded.
+//TO.DO  Use some useEffects and create loaders idk and load data only call is done, maybe use https://www.npmjs.com/package/react-loading-skeleton or similar packages.
+//TO.DO  Could move code bellow to a defs and utils/helpers .js file(s)
+
+const FETCH_ACCOUNT_URL = 'http://localhost:3333/api/account';
+
 const MONTH_NAMES = [ "January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December" ];
 
+const getYearsSincePurchase = (originalPurchasePriceDate) => {
+  return new Date().getFullYear() - new Date(originalPurchasePriceDate).getFullYear();
+};
 
+const getSincePurchase = (recentValuation, originalPurchasePrice) => {
+  return recentValuation.amount - originalPurchasePrice;
+};
+
+const getSincePurchasePercentage = (recentValuation, originalPurchasePrice) => {
+  return (getSincePurchase(recentValuation, originalPurchasePrice) / originalPurchasePrice) * 100
+};
+
+const renderCustomAccountListItem = (originalPurchasePriceDate, originalPriceValue) => {
+  return (<AccountList>
+    <AccountListItem>
+      <>
+        <InfoText>{`Purchased for`}&nbsp;</InfoText>
+        <InfoText><strong>{originalPriceValue}</strong></InfoText>
+        <InfoText>&nbsp;{`in ${MONTH_NAMES[new Date(originalPurchasePriceDate).getMonth()-1]} ${new Date(originalPurchasePriceDate).getFullYear()}`}</InfoText>
+      </>
+    </AccountListItem>
+    <AccountListItem><InfoText>Since Purchase</InfoText></AccountListItem>
+    <AccountListItem><InfoText>Annual Appreciation</InfoText></AccountListItem>
+  </AccountList>)
+};
+
+//this would be a more agnostic approach component in terms of business logic :)
+const TitledList = ({labels, title, headLineInfo = null, handleClick = () => null}) => (
+  <AccountSection>
+    <AccountLabel>{title} </AccountLabel>
+    {headLineInfo && <AccountHeadline>
+      {headLineInfo}
+    </AccountHeadline>}
+    <RowContainer onClick={handleClick}>
+      <AccountList>
+        {labels.map((label, key) => <AccountListItem key={key}><InfoText>{label}</InfoText></AccountListItem>)}
+      </AccountList>
+    </RowContainer>
+  </AccountSection>
+
+);
+//TO.DO: Create the business component that hides agnostic component also for product details and for product estimated value.
+const MortgageSection = ({currentBalance, name, handleClickMortgage}) => (
+  <TitledList title='Mortgage' labels={[currentBalance, name]} handleClick={handleClickMortgage}/>
+);
 
 const ValuationChangeSection = (account)=> {
-  //TO.DO: Not sure about the calculations. the business logic is not quite clear. might need changes
-  //TO.DO: Might move bellow computations in utils files or helpers
-  //TO.DO: It feels like the jsx tree/code is to verbose the components can be simplified and can we can reduce the code duplication by creating some section components like <PropertyDetails /> <ValuationChange/> and <Mortgage/>
-  //TO.DO: Instead of using the AccountList and AccountListItem I would create something more general and not business related components like only a Layout and a LayoutItem component and based on that create multy dimentional matrix layout
-  //TO.DO  For more minimized layout you could grop the data in structures like array and loop over them to minimize the code in the jsx. it would require to many changes now for this component since you have that price bolded, and i developed the code in the weekend. sorry, I don't have so much time now
-  //TO.DO  Use some useEffects and create loaders idk and load data only call is done, maybe use https://www.npmjs.com/package/react-loading-skeleton or similar packages.
-  //etc...
-  const yearsSincePurchase = new Date().getFullYear() - new Date(account.originalPurchasePriceDate).getFullYear();
-  const sincePurchase = account.recentValuation.amount - account.originalPurchasePrice;
-  const sincePurchasePercentage = (sincePurchase / account.originalPurchasePrice) * 100;
-  const annualAppreciationInfo =  `${sincePurchasePercentage / yearsSincePurchase}%`
+  const {recentValuation, originalPurchasePrice, originalPurchasePriceDate} = account;
+  const yearsSincePurchase = getYearsSincePurchase(originalPurchasePriceDate);
+
+  const sincePurchasePercentage = getSincePurchasePercentage(recentValuation, originalPurchasePrice);
 
   const originalPriceValue = new Intl.NumberFormat("en-GB", {
     style: "currency",
     currency: "GBP",
     maximumSignificantDigits: 3
   }).format(
-    Math.abs(account.originalPurchasePrice)
+    Math.abs(originalPurchasePrice)
   )
 
   const sincePurchaseValue = new Intl.NumberFormat("en-GB", {
@@ -41,33 +85,24 @@ const ValuationChangeSection = (account)=> {
     currency: "GBP",
     maximumSignificantDigits: 3
   }).format(
-    Math.abs(sincePurchase)
+    Math.abs(getSincePurchase(recentValuation, originalPurchasePrice))
   )
+
 
   const sincePurchaseInfo = `${sincePurchaseValue} (${sincePurchasePercentage}%)`;
   return (
-    <>
       <AccountSection>
         <AccountLabel>Valuation change</AccountLabel>
         <SectionWrapper>
-          <AccountList>
-            <AccountListItem>
-              <InfoText>{`Purchased for`}&nbsp;</InfoText>
-              <InfoText><strong>{originalPriceValue}</strong></InfoText>
-              <InfoText>&nbsp;{`in ${MONTH_NAMES[new Date(account.originalPurchasePriceDate).getMonth()-1]} ${new Date(account.originalPurchasePriceDate).getFullYear()}`}</InfoText>
-            </AccountListItem>
-            <AccountListItem><InfoText>Since Purchase</InfoText></AccountListItem>
-            <AccountListItem><InfoText>Annual Appreciation</InfoText></AccountListItem>
-          </AccountList>
+          {renderCustomAccountListItem(originalPurchasePriceDate, originalPriceValue)}
           <div>
             <AccountList>
               <AccountListItem skipRow goRight><InfoText isBadge>{sincePurchaseInfo}</InfoText></AccountListItem>
-              <AccountListItem goRight><InfoText isBadge>{annualAppreciationInfo}</InfoText></AccountListItem>
+              <AccountListItem goRight><InfoText isBadge>{`${sincePurchasePercentage / yearsSincePurchase}%`}</InfoText></AccountListItem>
             </AccountList>
           </div>
         </SectionWrapper>
       </AccountSection>
-    </>
     )
 }
 
@@ -77,16 +112,27 @@ const Detail = ({}) => {
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:3333/api/account').then((response) => {
+    axios.get(FETCH_ACCOUNT_URL).then((response) => {
       setAccount(response.data.account);
     });
   }, []);
 
-  let mortgage;
-  const lastUpdate = new Date(account?.lastUpdate);
-  if (account?.associatedMortgages.length) {
-    mortgage = account.associatedMortgages[0];
+  let lastUpdateRaw, currentBalance;
+  let mortgages = [];
+  if(account) {
+    const { associatedMortgages, lastUpdate} = account;
+    mortgages = associatedMortgages;
+    lastUpdateRaw = new Date(lastUpdate);
+
+    currentBalance = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP",
+    }).format(
+      Math.abs(mortgages[0]?.currentBalance)
+    );
   }
+
+
   return (
     <Inset>
       {account ? (
@@ -101,51 +147,37 @@ const Detail = ({}) => {
             </AccountHeadline>
             <AccountList>
               <AccountListItem><InfoText>
-                {`Last updated ${format(lastUpdate, "do MMM yyyy")}`}
+                {`Last updated ${format(lastUpdateRaw, "do MMM yyyy")}`}
               </InfoText></AccountListItem>
               <AccountListItem><InfoText>
                 {`Next update ${format(
-                  add(lastUpdate, { days: account.updateAfterDays }),
+                  add(lastUpdateRaw, { days: account.updateAfterDays }),
                   "do MMM yyyy"
                 )}`}
               </InfoText></AccountListItem>
             </AccountList>
           </AccountSection>
-          <AccountSection>
-            <AccountLabel>Property details</AccountLabel>
-            <RowContainer>
-              <AccountList>
-                <AccountListItem><InfoText>{account.name}</InfoText></AccountListItem>
-                <AccountListItem><InfoText>{account.bankName}</InfoText></AccountListItem>
-                <AccountListItem><InfoText>{account.postcode}</InfoText></AccountListItem>
-              </AccountList>
-            </RowContainer>
-          </AccountSection>
-          {account && <ValuationChangeSection
+          <TitledList
+            title={'Estimated Value'}
+            labels={[`Last updated ${format(lastUpdateRaw, "do MMM yyyy")}`, `Next update ${format(
+            add(lastUpdateRaw, { days: account.updateAfterDays }),
+            "do MMM yyyy"
+          )}`]}
+            headLineInfo={new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP",
+          }).format(account.recentValuation.amount)} />
+
+          <TitledList
+            title={'Property details'}
+            labels={[account.name, account.bankName, account.postcode]} />
+          <ValuationChangeSection
             originalPurchasePrice={account.originalPurchasePrice}
             originalPurchasePriceDate={account.originalPurchasePriceDate}
             recentValuation={account.recentValuation}
-          />}
-          {mortgage && (
-            <AccountSection>
-              <AccountLabel>Mortgage</AccountLabel>
-              <RowContainer
-                // This is a dummy action
-                onClick={() => alert("You have navigated to the mortgage page")}
-              >
-                <AccountList>
-                  <AccountListItem><InfoText>
-                    {new Intl.NumberFormat("en-GB", {
-                      style: "currency",
-                      currency: "GBP",
-                    }).format(
-                      Math.abs(account.associatedMortgages[0].currentBalance)
-                    )}
-                  </InfoText></AccountListItem>
-                  <AccountListItem><InfoText>{account.associatedMortgages[0].name}</InfoText></AccountListItem>
-                </AccountList>
-              </RowContainer>
-            </AccountSection>
+          />
+          {!!mortgages.length && (
+            <MortgageSection handleClickMortgage={() => alert("You have navigated to the mortgage page")} currentBalance={currentBalance} name={mortgages[0].name}/>
           )}
           <Button
             // This is a dummy action
@@ -164,5 +196,18 @@ ValuationChangeSection.propTypes = {
   originalPurchasePriceDate: PropTypes.string,
   recentValuation: PropTypes.object,
 };
+
+renderCustomAccountListItem.propTypes = {
+  originalPurchasePrice: PropTypes.number,
+  originalPurchasePriceDate: PropTypes.string
+};
+
+TitledList.propTypes = {
+  labels: PropTypes.array, // stirng[]
+  title: PropTypes.string,
+  headLineInfo: PropTypes.string,
+  handleClick: PropTypes.func
+};
+//etc
 
 export default Detail;
